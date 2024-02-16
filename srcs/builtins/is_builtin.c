fb10862b630745d41d20ccbb8b7e69a769de2dfa
loup <loup.quehec@gmail.com>
2024-02-16 10:50:52 +0100

    update stuff

--- srcs/builtins/is_builtin.c
+++ srcs/builtins/is_builtin.c
@@ -6,7 +6,7 @@
 /*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 08:25:49 by lquehec           #+#    #+#             */
-/*   Updated: 2024/02/16 09:07:45 by lquehec          ###   ########.fr       */
+/*   Updated: 2024/02/16 10:50:36 by lquehec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int	is_builtin(t_mini *mini, char *cmd, char **args)
 	else if (ft_strcmp(cmd, "export") == 0)
 		return (export_builtin(mini, cmd, args), 1);
 	else if (ft_strcmp(cmd, "unset") == 0)
-		return (unset_builtin(mini, cmd, args), 0);
+		return (unset_builtin(mini, cmd, args), 1);
 	else if (ft_strcmp(cmd, "env") == 0)
-		return (env_builtin(mini, cmd, args), 0);
+		return (env_builtin(mini, cmd, args), 1);
 	else if (ft_strcmp(cmd, "exit") == 0)
-		return (exit_builtin(mini, cmd, args), 0);
+		return (exit_builtin(mini, cmd, args), 1);
 	if (ft_lstsize_env(mini->env) == 0)
 		mini->exec_status = 127;
 	return (0);
