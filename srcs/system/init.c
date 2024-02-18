cbde10249ef371064c36781f41089fe1e3907b0c
loup <loup.quehec@gmail.com>
2024-02-18 14:37:08 +0100

    add dup at init for cleaning stdin and out for minishell

--- srcs/system/init.c
+++ srcs/system/init.c
@@ -6,7 +6,7 @@
 /*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 12:14:28 by lquehec           #+#    #+#             */
-/*   Updated: 2024/02/18 12:43:33 by lquehec          ###   ########.fr       */
+/*   Updated: 2024/02/18 14:36:32 by lquehec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,12 @@ void	init_mini(t_mini *mini, char **env)
 	mini->last_exec_status = EXEC_SUCCESS;
 	mini->exec_status = EXEC_SUCCESS;
 	mini->exit_code = -1;
-	
-	// print_env(mini); // -> DEBUG
-	// mini->std_in = dup(STDIN_FILENO);
-	// if (mini->std_in == -1)
-	// 	ft_exit(mini, EXIT_FAILURE, ERR_DUP);
-	// mini->std_out = dup(STDOUT_FILENO);
-	// if (mini->std_out == -1)
-	// 	ft_exit(mini, EXIT_FAILURE, ERR_DUP);
+	mini->std_in = dup(STDIN_FILENO);
+	if (mini->std_in == -1)
+		ft_exit(mini);
+	mini->std_out = dup(STDOUT_FILENO);
+	if (mini->std_out == -1)
+		ft_exit(mini);
 	env_init(mini, env);
 }
 
