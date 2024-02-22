a17b43e95f483fb12871ec9a8c711c304e49a077
Loup Quehec <lquehec@bess-f3r3s14.clusters.42paris.fr>
2024-02-22 18:32:33 +0100

    fiux so many stuff

--- srcs/signals/signals_parent.c
+++ srcs/signals/signals_parent.c
@@ -6,7 +6,7 @@
 /*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 11:28:00 by lquehec           #+#    #+#             */
-/*   Updated: 2024/02/20 11:43:41 by lquehec          ###   ########.fr       */
+/*   Updated: 2024/02/22 18:29:19 by lquehec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,17 @@ static void	sig_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
+		g_status = 130;
 		write(1, "\n", 1);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
 	}
 	else if (sig == SIGQUIT)
 	{
-		rl_on_new_line();
-		rl_redisplay();
-		ft_putstr_nl(MSG_EXIT);
+		g_status = 131;
+		ft_dprintf("Quit (core dumped)\n");
 	}
 }
 
-static void	signal_ctrl_c_parent(void)
+void	signal_ctrl_c_parent(void)
 {
 	struct sigaction	ctrl_c;
 
@@ -43,7 +40,7 @@ static void	signal_ctrl_backslash(void)
 {
 	struct sigaction	ctrl_backslash;
 
-	ctrl_backslash.sa_handler = SIG_IGN;
+	ctrl_backslash.sa_handler = sig_handler;
 	ctrl_backslash.sa_flags = SA_RESTART;
 	sigemptyset(&ctrl_backslash.sa_mask);
 	sigaction(SIGQUIT, &ctrl_backslash, NULL);
